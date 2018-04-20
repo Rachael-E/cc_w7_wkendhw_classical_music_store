dc1c13014ce9adc21d1b384aecb427827c783dd9
Rachael <rachael.ellen@btopenworld.com>
2018-04-20 11:51:29 +0100

    tests for instruments completed and passing

--- src/test/java/InstrumentsTest.java
+++ src/test/java/InstrumentsTest.java
@@ -23,4 +23,24 @@ public class InstrumentsTest {
         assertEquals(750, clarinet1.getBoughtPrice(), 0.1);
     }
 
+    @Test
+    public void clarinetHasSellPrice(){
+        assertEquals(1000, clarinet1.getSellPrice(), 0.1);
+    }
+
+    @Test
+    public void clarinetHasInstrumentType(){
+        assertEquals(InstrumentType.CLARINET, clarinet1.getInstrumentType());
+    }
+
+    @Test
+    public void clarinetCanPlay(){
+        assertEquals("I am the main star of Gerschwin, Rhapsody in Blue!", clarinet1.play());
+    }
+
+    @Test
+    public void clarinetHasMarkUp(){
+        assertEquals(250, clarinet1.calculateMarkup(), 0.1);
+    }
+
 }
